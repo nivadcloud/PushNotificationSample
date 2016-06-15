ba8a9670183c53dddaa90461243ffe33b8d1e601
Parsoa <parsoakhorsand91@gmail.com>
2016-06-15 17:56:09 +0430

    initial commit

---
+++ app/src/main/java/io/nivad/nivadpushnotificationssample/MainActivity.java
@@ -0,0 +1,32 @@
+package io.nivad.nivadpushnotificationssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.nivad.push.Facade.Facade;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button register = (Button) findViewById(R.id.activity_main_button_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Facade is the interface provided by Nivad for accessing its features
+                        Facade.registerGCM(MainActivity.this);
+                    }
+                }).start();
+            }
+        });
+    }
+
+}
